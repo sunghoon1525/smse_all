65128922ce11d10adeff83261967b039321e038a
sunghoon <sunghoon6639@gmail.com>
2022-11-09 14:25:48 +0900

    check github

---
+++ app/src/main/java/kr/ac/pknu/smse/smse_all/MainActivity.java
@@ -0,0 +1,15 @@
+package kr.ac.pknu.smse.smse_all;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+//연동 확인
