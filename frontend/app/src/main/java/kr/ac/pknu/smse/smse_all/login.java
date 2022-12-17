09b08379de5415cb34aab010301a24847e9e6090
82104 <oaoa13@pukyong.ac.kr>
2022-12-17 16:07:50 +0900

    login backend 함수 작성 완료

--- frontend/app/src/main/java/kr/ac/pknu/smse/smse_all/login.java
+++ frontend/app/src/main/java/kr/ac/pknu/smse/smse_all/login.java
@@ -17,7 +17,8 @@ public class login extends AppCompatActivity {
         //입력받은 아이디와 비밀번호를 변수에 저장
         inputId = (EditText) findViewById(R.id.edittext3);
         inputPw = (EditText) findViewById(R.id.edittext4);
-        
+        // 이거를 파이썬 함수에 넣어서 true면 넘어가고 아니면 다음페이지로 못넘어가게 해주실수있나요
+        // 파이썬 함수를 불러오는건 따로 뭐 설치해야함
     final ImageButton signup1 =(ImageButton)findViewById(R.id.imagebutton3);
         signup1.setOnClickListener(new View.OnClickListener() {
 
