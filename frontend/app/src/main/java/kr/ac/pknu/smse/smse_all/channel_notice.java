069ef5bb5b494e1cabf24793e15ece10d0edf137
sunghoon <sunghoon6639@gmail.com>
2022-12-17 16:07:48 +0900

    12-17-01

---
+++ frontend/app/src/main/java/kr/ac/pknu/smse/smse_all/channel_notice.java
@@ -0,0 +1,28 @@
+package kr.ac.pknu.smse.channel01;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class channel_notice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.channel_notion);
+
+        final ImageButton backnotion =(ImageButton)findViewById(R.id.back_notion);
+        backnotion.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(channel_notice.this, channel_main.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
