bcbf8c500745b6c16c46b07f8d31e104dad11cc9
82104 <oaoa13@pukyong.ac.kr>
2022-12-17 16:21:53 +0900

    주석없애고다시도전

--- frontend/app/src/main/java/kr/ac/pknu/smse/smse_all/login.java
+++ frontend/app/src/main/java/kr/ac/pknu/smse/smse_all/login.java
@@ -1,4 +1,4 @@
-package com.example.smse;
+package kr.ac.pknu.smse.smse_all;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,41 +13,16 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        //입력받은 아이디와 비밀번호를 변수에 저장
-        inputId = (EditText) findViewById(R.id.edittext3);
-        inputPw = (EditText) findViewById(R.id.edittext4);
-        // 이거를 파이썬 함수에 넣어서 true면 넘어가고 아니면 다음페이지로 못넘어가게 해주실수있나요
-        // 파이썬 함수를 불러오는건 따로 뭐 설치해야함
-
-        final ImageButton signup1 = (ImageButton) findViewById(R.id.imagebutton3);
+    final ImageButton signup1 =(ImageButton)findViewById(R.id.imagebutton3);
         signup1.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(login.this, signup1.class);
-                startActivity(intent);
-            }
-        });
-        final ImageButton findid = (ImageButton) findViewById(R.id.imagebutton1);
-        findid.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+            Intent intent = new Intent(login.this, signup1.class);
+            startActivity(intent);
+            // 입력받은 아이디와 비밀번호를 변수에 저장해야함.
+        }
+    });
+      }
 
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(login.this, findid.class);
-                startActivity(intent);
-            }
-        });
-
-        final ImageButton findpw = (ImageButton) findViewById(R.id.imagebutton2);
-        findpw.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(login.this, findpassword.class);
-                startActivity(intent);
-            }
-        });
     }
-}
-
