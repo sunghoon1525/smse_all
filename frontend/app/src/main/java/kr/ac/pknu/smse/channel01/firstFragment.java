1cd00222e4063b90df2f03c71be6c049d382cfc3
sunghoon <sunghoon6639@gmail.com>
2022-12-19 00:07:27 +0900

    good

---
+++ frontend/app/src/main/java/kr/ac/pknu/smse/channel01/firstFragment.java
@@ -0,0 +1,19 @@
+package kr.ac.pknu.smse.channel01;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class MainMenuChartFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.mypage_main, container, false);
+
+        return rootView;
+    }
+}
---
+++ frontend/app/src/main/java/kr/ac/pknu/smse/channel01/thirdFragment.java
@@ -0,0 +1,20 @@
+package kr.ac.pknu.smse.channel01;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class thirdFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.mypage_main, container, false);
+
+        return rootView;
+    }
+}
