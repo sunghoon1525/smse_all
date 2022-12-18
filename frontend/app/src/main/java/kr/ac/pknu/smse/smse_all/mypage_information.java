ab1043959e08a5f25a37c95acc7122ba6c621762
sunghoon <sunghoon6639@gmail.com>
2022-12-18 21:46:05 +0900

    no

---
+++ frontend/app/src/main/java/kr/ac/pknu/smse/smse_all/mypage_information.java
@@ -0,0 +1,14 @@
+package kr.ac.pknu.smse.channel01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mypage_information extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mypage_information);
+    }
+}
