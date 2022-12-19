8f679d66095b36a5423a4e42543d664811d5f5a9
leedidu <99534272+leedidu@users.noreply.github.com>
2022-12-19 19:50:03 +0900

    Update db_connector.py

--- backend/__pycache__/db_connector.py
+++ backend/__pycache__/db_connector.py
@@ -3,7 +3,7 @@ import pymysql
 # MySQL Connection 연결
 conn = pymysql.connect(host='localhost', user='root', password='12345678', db='SMSE_ALL', charset='utf8')
 
-# Connection 으로부터 Cursor 생성
+# Connection 으로부터 Cursor 생성 
 curs = conn.cursor()
 
 # SQL문 실행
